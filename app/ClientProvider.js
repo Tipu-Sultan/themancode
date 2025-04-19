aa6c45de4d3020984162c98a3586119fd99e147d
Tipu-Sultan <teepukhan729@gmail.com>
2025-04-19 23:56:15 +0530

    update

--- app/ClientProvider.js
+++ app/ClientProvider.js
@@ -3,10 +3,14 @@
 import { SessionProvider } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+const siteKey = process.env.RECAPTCHA_SITE_KEY;
+if (!siteKey) {
+  throw new Error('Please define the RECAPTCHA_SITE_KEY environment variable');
+}
 
 export default function ClientProvider({ children }) {
-  const siteKey = process.env.RECAPTCHA_SITE_KEY || '';
   return (
+    
     <SessionProvider>
       <GoogleReCaptchaProvider reCaptchaKey={siteKey}>
         {children}
